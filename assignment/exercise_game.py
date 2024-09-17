29feef84963d157da25ae84f64d53c0044da2408
Caelan Wong <113719791+Caelanrw@users.noreply.github.com>
2024-09-17 16:06:12 -0400

    Update game code to check for authorized user, play 10 times, and upload data to the cloud

--- assignment/exercise_game.py
+++ assignment/exercise_game.py
@@ -6,9 +6,17 @@ from machine import Pin
 import time
 import random
 import json
+import requests
+import sys
+
+database_api_url = "https://senior-design-a96f3-default-rtdb.firebaseio.com/data.json"
+#flask_server_url = "http://192.168.0.74:1111/post_data"
+api_key = "AIzaSyDSfGMO0UbvSRRcCq0SgVQ82493k5_G1iM"
+auth_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + api_key
 
 
-N: int = 3
+
+N: int = 10
 sample_ms = 10.0
 on_ms = 500
 
@@ -45,7 +53,7 @@ def write_json(json_filename: str, data: dict) -> None:
         json.dump(data, f)
 
 
-def scorer(t: list[int | None]) -> None:
+def scorer(t: list[int | None], idToken) -> None:
     # %% collate results
     misses = t.count(None)
     print(f"You missed the light {misses} / {len(t)} times")
@@ -57,8 +65,14 @@ def scorer(t: list[int | None]) -> None:
     # add key, value to this dict to store the minimum, maximum, average response time
     # and score (non-misses / total flashes) i.e. the score a floating point number
     # is in range [0..1]
-    data = {}
-
+    add = 0
+    for i in range(len(t_good)):
+      add+=t_good[i]
+    data = {'minimum': min(t_good), 'maximum':max(t_good),'AvgResponseTime':add/len(t_good),'score':float((len(t)-misses)/len(t))}
+    print(data['minimum'])
+    print(data['maximum'])
+    print(data['AvgResponseTime'])
+    print(data['score'])
     # %% make dynamic filename and write JSON
 
     now: tuple[int] = time.localtime()
@@ -69,17 +83,31 @@ def scorer(t: list[int | None]) -> None:
     print("write", filename)
 
     write_json(filename, data)
-
+    
+    response = requests.post(database_api_url, json=data)
+    #response = requests.post(flask_server_url, json=data)
 
 if __name__ == "__main__":
     # using "if __name__" allows us to reuse functions in other script files
 
     led = Pin("LED", Pin.OUT)
-    button = Pin(16, Pin.IN, Pin.PULL_UP)
+    button = Pin(11, Pin.IN, Pin.PULL_UP)
 
     t: list[int | None] = []
 
     blinker(3, led)
+    
+    user_email = input("Please enter your email: ")
+    user_password = input("Please enter your password: ")
+    response = requests.post(auth_url, json={"email":user_email,"password":user_password,"returnSecureToken":True})
+    response_data = response.json()
+    if "idToken" in response_data:
+        print("Authenticated")
+        print("Start playing")
+        blinker(3,led)
+    else:
+        print("Authentication failed.")
+        sys.exit(0)
 
     for i in range(N):
         time.sleep(random_time_interval(0.5, 5.0))
@@ -99,4 +127,4 @@ if __name__ == "__main__":
 
     blinker(5, led)
 
-    scorer(t)
+    scorer(t,response_data["idToken"])
